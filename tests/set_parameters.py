18377705e04a81ccf3fead24ab95346fd3558d39
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-06-18 17:19:01 +0200

    test: add test parameters config

---
+++ tests/set_parameters.py
@@ -0,0 +1,13 @@
+'''
+  Copyright 2024 KU Leuven
+  Ryan Antonio <ryan.antonio@esat.kuleuven.be>
+
+  Description:
+  This contains the parameters used across all tests.
+'''
+
+# Test Parameters
+TEST_RUNS = 100
+
+# Working dimensions
+HV_DIM = 512
