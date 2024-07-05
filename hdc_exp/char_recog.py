312cc4ac3d2aecdb45a0e2d3b7cf52410a971e70
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-07-05 12:57:51 +0200

    exp: upgrade test to include CA90

--- hdc_exp/char_recog.py
+++ hdc_exp/char_recog.py
@@ -17,6 +17,8 @@ from hdc_util import (
     extract_dataset,
     measure_acc,
     prediction_set,
+    simple_plot2d,
+    gen_ri_hv,
 )
 from tqdm import tqdm
 import numpy as np
@@ -39,12 +41,14 @@ IMG_PIXEL_NUM = IMG_ROW_NUM * IMG_COL_NUM
 
 # Characteristics of HV
 HV_TYPE = "binary"
-HV_DIM = 128
+HV_DIM = 512
 P_DENSE = 0.5
+HV_SEED_DIM = HV_DIM
+HV_SEED_DENSE = 1 / 2
 THRESHOLD = np.floor(IMG_ROW_NUM * IMG_COL_NUM / 2)
 
 # Test parameters
-TEST_RUNS = 50
+TEST_RUNS = 10
 
 
 """
@@ -113,12 +117,8 @@ def encode_character(hv_dim, ortho_im, character_input, threshold, hv_type="bina
     return char_hv
 
 
-# Main function to run the character recognition program
-if __name__ == "__main__":
-    # Get original data set
-    file_path = "./data_set/char_recog/characters.txt"
-    dataset = extract_dataset(file_path)
-
+# Entire characte recognition run
+def run_char_recog(dataset, max_distort, test_runs, hv_seed_dim, im_gen="random"):
     # Minor data clean to make the data a list
     # of binary numbers instead of characters
     for i in range(len(dataset)):
@@ -128,8 +128,20 @@ if __name__ == "__main__":
     correct_set = list(range(NUM_CLASSSES))
 
     # Generate base hypervectors
+    # Depending on the type of generation
+    if im_gen == "random":
+        hv_seed = 0
+    else:
+        # This case covers CA 90
+        hv_seed = gen_ri_hv(hv_seed_dim, P_DENSE)
+
     ortho_im = gen_orthogonal_im(
-        num_hv=IMG_PIXEL_NUM, hv_dim=HV_DIM, p_dense=P_DENSE, hv_type=HV_TYPE
+        num_hv=IMG_PIXEL_NUM,
+        hv_dim=HV_DIM,
+        p_dense=P_DENSE,
+        hv_seed=hv_seed,
+        hv_type=HV_TYPE,
+        im_type=im_gen,
     )
 
     # Encode the characters into AM
@@ -142,13 +154,12 @@ if __name__ == "__main__":
 
     # This is the testing part
     # Test for different distortions
-    max_distort = 10
     avg_acc = np.zeros(max_distort)
 
     # This list is for progress bar purposes
-    test_run_list = list(range(TEST_RUNS))
+    test_run_list = list(range(test_runs))
 
-    # Do this for TEST_RUNS times
+    # Do this for test_runs times
     # Use tqdm for a progress bar display
     for num in tqdm(test_run_list, desc="Running tests: "):
         # Initialize accuracy list
@@ -178,8 +189,108 @@ if __name__ == "__main__":
         avg_acc = avg_acc + np.array(acc_list)
 
     # Average accuracy scores in multiple test runs
-    avg_acc = avg_acc / TEST_RUNS
+    avg_acc = avg_acc / test_runs
 
     print("Average accuracy:")
     for i in range(len(avg_acc)):
         print(f"Avg accuracy for distortion num {i}: {avg_acc[i]}")
+
+    return avg_acc
+
+
+# Main function to run the character recognition program
+if __name__ == "__main__":
+    # Get original data set
+    file_path = "./data_set/char_recog/characters.txt"
+    dataset = extract_dataset(file_path)
+
+    # Set max number of distortions
+    max_distort = 20
+
+    # For the x-axis plot later
+    distort_x_list = list(range(max_distort))
+
+    # Run the character recognition sets
+    # First get the normal item memory generation
+    avg_acc_random_im = run_char_recog(
+        dataset, max_distort, TEST_RUNS, hv_seed_dim=0, im_gen="random"
+    )
+
+    # Get the CA 90 dependent generations
+    avg_acc_ca90_iter_im_list = []
+    avg_acc_ca90_hier_im_list = []
+
+    for i in range(8):
+        # Run the character recognition per CA 90
+        avg_acc_ca90_iter_im = run_char_recog(
+            dataset,
+            max_distort,
+            TEST_RUNS,
+            hv_seed_dim=int(HV_DIM / (2**i)),
+            im_gen="ca90_iter",
+        )
+        avg_acc_ca90_hier_im = run_char_recog(
+            dataset,
+            max_distort,
+            TEST_RUNS,
+            hv_seed_dim=int(HV_DIM / (2**i)),
+            im_gen="ca90_hier",
+        )
+
+        # Save into the list
+        avg_acc_ca90_iter_im_list.append(avg_acc_ca90_iter_im)
+        avg_acc_ca90_hier_im_list.append(avg_acc_ca90_hier_im)
+
+    # List of y-axis value
+    avg_acc_list = [
+        avg_acc_random_im,
+        avg_acc_ca90_iter_im_list[0],
+        avg_acc_ca90_hier_im_list[0],
+        avg_acc_ca90_iter_im_list[1],
+        avg_acc_ca90_hier_im_list[1],
+        avg_acc_ca90_iter_im_list[2],
+        avg_acc_ca90_hier_im_list[2],
+        avg_acc_ca90_iter_im_list[3],
+        avg_acc_ca90_hier_im_list[3],
+        avg_acc_ca90_iter_im_list[4],
+        avg_acc_ca90_hier_im_list[4],
+        avg_acc_ca90_iter_im_list[5],
+        avg_acc_ca90_hier_im_list[5],
+        avg_acc_ca90_iter_im_list[6],
+        avg_acc_ca90_hier_im_list[6],
+        avg_acc_ca90_iter_im_list[7],
+        avg_acc_ca90_hier_im_list[7],
+    ]
+
+    # Label list
+    label_list = [
+        "Random",
+        "CA 90 Iterative",
+        "CA 90 Hierarchical",
+        "CA 90 Iterative Div2",
+        "CA 90 Hierarchical Div2",
+        "CA 90 Iterative Div4",
+        "CA 90 Hierarchical Div4",
+        "CA 90 Iterative Div8",
+        "CA 90 Hierarchical Div8",
+        "CA 90 Iterative Div16",
+        "CA 90 Hierarchical Div16",
+        "CA 90 Iterative Div32",
+        "CA 90 Hierarchical Div32",
+        "CA 90 Iterative Div64",
+        "CA 90 Hierarchical Div64",
+        "CA 90 Iterative Div128",
+        "CA 90 Hierarchical Div128",
+    ]
+
+    # Plot multiple lines
+    simple_plot2d(
+        x=distort_x_list,
+        y=avg_acc_list,
+        title="Accuracy vs Distortion",
+        xlabel="Distortion Number",
+        ylabel="Accuracy",
+        plt_label=label_list,
+        marker="o",
+        single_plot=False,
+    )
