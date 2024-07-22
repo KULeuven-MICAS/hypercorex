c55f533909dc993b6e1c2e4f8ec3e412e45559fe
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-07-22 10:56:11 +0200

    test: update ca90 item memory test

--- tests/test_ca90_item_memory.py
+++ tests/test_ca90_item_memory.py
@@ -13,13 +13,15 @@ from cocotb.triggers import Timer
 import pytest
 import sys
 
-from util import get_root, setup_and_run, check_result_array, numbin2list, hvlist2num
+from util import get_root, setup_and_run, check_result_array, numbin2list
 
 # Add hdc utility functions
 hdc_util_path = get_root() + "/hdc_exp/"
 sys.path.append(hdc_util_path)
 
-from hdc_util import gen_orthogonal_im, gen_ri_hv  # noqa: E402
+# Grab the CA90 generation from the
+# cellular automata experiment set
+from cellular_automata import gen_ca90_im_set  # noqa: E402
 
 
 @cocotb.test()
@@ -28,30 +30,45 @@ async def ca90_item_memory_dut(dut):
     cocotb.log.info("         Testing CA90 Item Memory           ")
     cocotb.log.info(" ------------------------------------------ ")
 
-    # Get nice seed with 50% 1s and 0s
-    seed_hv_list = gen_ri_hv(set_parameters.HV_DIM, 0.5)
-    seed_hv = hvlist2num(seed_hv_list)
+    # For parameter checking, we put a warning
+    if set_parameters.NUM_PER_IM_BANK >= int(set_parameters.HV_DIM / 2):
+        cocotb.log.info(" ------------------------------------------ ")
+        cocotb.log.info("            !!!!!  WARNING  !!!!!           ")
+        cocotb.log.info(f" The number of IM per bank {set_parameters.NUM_PER_IM_BANK}")
+        cocotb.log.info(
+            f" must be less than half of the HV dimension size {set_parameters.HV_DIM}"
+        )
+        cocotb.log.info(
+            " It is recommended that it is 1/4th of the dimension size to avoid"
+        )
+        cocotb.log.info(" saturating at all 1s or all 0s due to CA90 limitations")
+        cocotb.log.info(" ------------------------------------------ ")
 
-    cocotb.log.info(f"Working with seed: {seed_hv}")
-
-    # Generate golden item memory
-    golden_im = gen_orthogonal_im(
-        set_parameters.NUM_ORTHO_ITEMS,
-        set_parameters.HV_DIM,
-        0.5,
-        seed_hv_list,
-        hv_type="binary",
-        im_type="ca90_iter",
+    # Generate seed list and golden IM
+    seed_list, golden_im, conf_mat = gen_ca90_im_set(
+        seed_size=set_parameters.REG_FILE_WIDTH,
+        hv_dim=set_parameters.HV_DIM,
+        num_total_im=set_parameters.NUM_TOT_IM,
+        num_per_im_bank=set_parameters.NUM_PER_IM_BANK,
+        ca90_mode=set_parameters.CA90_MODE,
     )
 
-    # Dummy seed value
-    dut.seed_hv_i.value = seed_hv
+    # For combining into a single
+    # wire bus for simulation purposes
+    num_im_banks = int(set_parameters.NUM_TOT_IM / set_parameters.NUM_PER_IM_BANK)
+    seed_input = 0
+    for i in range(num_im_banks):
+        seed_input = (seed_input << set_parameters.REG_FILE_WIDTH) + seed_list[
+            num_im_banks - i - 1
+        ]
+
+    dut.seed_hv_i.value = seed_input
 
     # Propagate time for logic
     await Timer(1, units="ps")
 
     # Load request, propagate, check result
-    for i in range(set_parameters.NUM_ORTHO_ITEMS):
+    for i in range(set_parameters.NUM_TOT_IM):
         dut.im_sel_a_i.value = i
         dut.im_sel_b_i.value = i
         await Timer(1, units="ps")
@@ -81,13 +98,15 @@ async def ca90_item_memory_dut(dut):
     [
         {
             "HVDimension": str(set_parameters.HV_DIM),
-            "NumImElements": str(set_parameters.NUM_ORTHO_ITEMS),
+            "NumTotIm": str(set_parameters.NUM_TOT_IM),
+            "NumPerImBank": str(set_parameters.NUM_PER_IM_BANK),
         }
     ],
 )
 def test_ca90_item_memory(simulator, parameters):
     verilog_sources = [
         "/rtl/item_memory/ca90_unit.sv",
+        "/rtl/item_memory/ca90_hier_base.sv",
         "/rtl/item_memory/ca90_item_memory.sv",
     ]
 
@@ -101,5 +120,4 @@ def test_ca90_item_memory(simulator, parameters):
         module=module,
         simulator=simulator,
         parameters=parameters,
-        waves=True,
     )
