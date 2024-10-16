62a22ed288a2d433577a07c870d228e53e2ed275
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-10-16 18:08:26 +0200

    hw: update the update counter

--- rtl/data_formatter/update_counter.sv
+++ rtl/data_formatter/update_counter.sv
@@ -13,20 +13,19 @@
 //---------------------------
 
 module update_counter #(
-  parameter int unsigned CounterWidth = 32,
-  parameter int unsigned NumTotIm    = 1024,
-  // Don't touch!
-  parameter int unsigned ImAddrWidth = $clog2(NumTotIm)
+  parameter int unsigned CsrDataWidth = 32
 )(
   // Clocks and reset
-  input  logic clk_i,
-  input  logic rst_ni,
+  input  logic                    clk_i,
+  input  logic                    rst_ni,
   // Inputs
-  input  logic en_i,
-  input  logic clr_i,
-  input  logic [CounterWidth-1:0] max_count_i,
+  input  logic                    en_i,
+  input  logic                    clr_i,
+  input  logic                    start_i,
+  input  logic [CsrDataWidth-1:0] max_count_i,
+  input  logic [CsrDataWidth-1:0] start_count_i,
   // Outputs
-  output logic [ImAddrWidth-1:0]  addr_o,
+  output logic [CsrDataWidth-1:0] addr_o,
   output logic                    addr_valid_o,
   input  logic                    addr_ready_i
 );
@@ -34,7 +33,7 @@ module update_counter #(
   //---------------------------
   // Wires
   //---------------------------
-  logic [CounterWidth-1:0] addr_reg;
+  logic [CsrDataWidth-1:0] addr_reg;
 
   logic max_count_hit;
   logic max_count_success;
@@ -44,9 +43,8 @@ module update_counter #(
   //---------------------------
   // Combinational Logic
   //---------------------------
-
   assign addr_success = addr_valid_o & addr_ready_i;
-  assign max_count_hit = addr_reg == max_count_i-1;
+  assign max_count_hit = addr_reg == (max_count_i + start_count_i)-1;
   assign max_count_success = max_count_hit & addr_success;
 
   //---------------------------
@@ -54,18 +52,18 @@ module update_counter #(
   //---------------------------
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      addr_reg <= {CounterWidth{1'b0}};
+      addr_reg <= {CsrDataWidth{1'b0}};
     end else begin
       if (en_i) begin
-        if(clr_i || max_count_success) begin
-          addr_reg <= {CounterWidth{1'b0}};
+        if(clr_i || max_count_success || start_i) begin
+          addr_reg <= start_count_i;
         end else if(addr_success) begin
           addr_reg <= addr_reg + 1;
         end else begin
           addr_reg <= addr_reg;
         end
       end else begin
-        addr_reg <= {CounterWidth{1'b0}};
+        addr_reg <= {CsrDataWidth{1'b0}};
       end
     end
   end
@@ -73,7 +71,7 @@ module update_counter #(
   //---------------------------
   // Output
   //---------------------------
-  assign addr_o       = addr_reg[ImAddrWidth-1:0];
+  assign addr_o       = addr_reg;
   assign addr_valid_o = en_i;
 
 endmodule
