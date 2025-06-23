5335dff4ec59b6f929419406444b4cfd51110b3a
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-06-23 23:47:06 +0200

    hdc_exp: refactor digit_recog

--- hdc_exp/digit_recog.py
+++ hdc_exp/digit_recog.py
@@ -11,14 +11,16 @@
 
 from hdc_util import (
     extract_git_dataset,
+    load_dataset,
+    train_model,
+    test_model,
+    retrain_model,
     gen_empty_hv,
     gen_orthogonal_im,
     bind_hv,
     binarize_hv,
-    prediction_idx,
     gen_ca90_im_set,
 )
-from tqdm import tqdm
 
 
 DATA_URL = "https://github.com/KULeuven-MICAS/hypercorex/releases/download/ds_hdc_digit_recog_v.0.0.1/digit_recog.tar.gz"
@@ -26,10 +28,11 @@ DATA_SET_DIR = "data_set"
 DATA_DIR = f"{DATA_SET_DIR}/digit_recog"
 
 
-def encode_image(image, ortho_im, num_features):
+def encode_digit(image, ortho_im, cim):
     # Encode image
     hv_dim = len(ortho_im[0])
     encoded_image = gen_empty_hv(hv_dim)
+    num_features = len(image)
     threshold = num_features / 2
 
     # Cycle through the entire line
@@ -46,145 +49,13 @@ def encode_image(image, ortho_im, num_features):
     return encoded_image
 
 
-def train_digit_recog_model(ortho_im, training_dir, num_train, num_features):
-    hv_dim = len(ortho_im[0])
-    train_threshold = num_train / 2
-
-    class_am = dict()
-    class_am_int = dict()
-    class_am_elem_count = dict()
-
-    for lang in range(10):
-        class_am[lang] = gen_empty_hv(hv_dim)
-
-    for digit in range(10):
-        # Training dataset
-        read_file = f"{training_dir}/bin_mnist_{digit}.txt"
-
-        image_lines = []
-        with open(read_file, "r") as rf:
-            for line in rf:
-                line = line.strip().split()
-                int_line = [int(x) for x in line]
-                image_lines.append(int_line)
-
-        class_hv = gen_empty_hv(len(ortho_im[0]))
-
-        for i in tqdm(range(NUM_TRAIN), desc=f"Training digit: {digit}"):
-            class_hv += encode_image(image_lines[i], ortho_im, num_features)
-
-        # Save non-binarized AM
-        class_am_int[digit] = class_hv
-
-        # Save binarized AM
-        class_hv = binarize_hv(class_hv, train_threshold, "binary")
-        class_am[digit] = class_hv
-
-        # Save threshold list
-        class_am_elem_count[digit] = num_train
-
-    return class_am, class_am_int, class_am_elem_count
-
-
-def retrain_digit_recog_model(
-    class_am,
-    class_am_int,
-    class_am_elem_count,
-    ortho_im,
-    training_dir,
-    num_classes,
-    num_retrain,
-    num_features,
-    staring_num_test,
-):
-    for digit in range(num_classes):
-        # Retraining dataset
-        read_file = f"{training_dir}/bin_mnist_{digit}.txt"
-
-        # Open images again
-        image_lines = []
-        with open(read_file, "r") as rf:
-            for line in rf:
-                line = line.strip().split()
-                int_line = [int(x) for x in line]
-                image_lines.append(int_line)
-
-        for i in tqdm(range(num_retrain), desc=f"Retraining digit: {digit}"):
-            # Get encodede image
-            encoded_line = encode_image(
-                image_lines[staring_num_test + i], ortho_im, num_features
-            )
-            # Get prediction
-            prediction = prediction_idx(class_am, encoded_line, hv_type="binary")
-
-            # Update AM for every incorrect prediction
-            if prediction != digit:
-                # Update the class AM
-                class_am_int[prediction] -= encoded_line
-                class_am_int[digit] += encoded_line
-
-                # Update the threshold
-                class_am_elem_count[prediction] -= 1
-                class_am_elem_count[digit] += 1
-
-    # After updating rebinarize the AM
-    for digit in range(num_classes):
-        # Save binarized AM
-        threshold = class_am_elem_count[digit] / 2
-        class_am[digit] = binarize_hv(class_am_int[digit], threshold, "binary")
-
-    return class_am, class_am_int, class_am_elem_count
-
-
-def test_digit_recog_model(
-    class_am, ortho_im, testing_dir, num_features, staring_num_test, num_test
-):
-    overall_count = 0
-    overall_score = 0
-
-    for digit in range(10):
-        # Training dataset
-        read_file = f"{testing_dir}/bin_mnist_{digit}.txt"
-
-        image_lines = []
-        with open(read_file, "r") as rf:
-            for line in rf:
-                line = line.strip().split()
-                int_line = [int(x) for x in line]
-                image_lines.append(int_line)
-
-        total_count = 0
-        total_score = 0
-
-        # Make a prediction
-        for i in tqdm(range(num_test), desc=f"Testing digit: {digit}"):
-            encoded_line = encode_image(
-                image_lines[staring_num_test + i], ortho_im, num_features
-            )
-            prediction = prediction_idx(class_am, encoded_line, hv_type="binary")
-            if prediction == digit:
-                total_score += 1
-            total_count += 1
-
-        accuracy = total_score / total_count if total_count > 0 else 0
-        print(f"Digit: {digit}, Accuracy: {accuracy:.2f}")
-
-        overall_score += total_score
-        overall_count += total_count
-
-    overall_accuracy = overall_score / overall_count if overall_count > 0 else 0
-    print(f"Overall Accuracy: {overall_accuracy:.2f}")
-
-    return
-
-
 if __name__ == "__main__":
     SEED_DIM = 32
-    HV_DIM = 512
+    HV_DIM = 4096
     NUM_TOT_IM = 1024
     NUM_PER_IM_BANK = 128
     NGRAM = 4
-    USE_CA90_IM = True
+    USE_CA90_IM = False
     EXTRACT_DATA = True
 
     NUM_CLASSES = 10
@@ -233,29 +104,69 @@ if __name__ == "__main__":
             im_type="random",
         )
 
-    # Training
-    class_am, class_am_int, class_am_elem_count = train_digit_recog_model(
-        ortho_im, DATA_DIR, NUM_TRAIN, NUM_FEATURES
+    print("Extracting data...")
+    train_data = dict()
+    for num_class in range(NUM_CLASSES):
+        # Training dataset
+        read_file = f"{DATA_DIR}/bin_mnist_{num_class}.txt"
+        train_data[num_class] = load_dataset(read_file)
+
+    # For simplicity use both train data
+    test_data = dict()
+    for num_class in range(NUM_CLASSES):
+        # Training dataset
+        read_file = f"{DATA_DIR}/bin_mnist_{num_class}.txt"
+        test_data[num_class] = load_dataset(read_file)
+
+    print("Training model...")
+    class_am, class_am_int, class_am_elem_count = train_model(
+        train_dataset=train_data,
+        num_train=NUM_TRAIN,
+        ortho_im=ortho_im,
+        cim=None,
+        encode_function=encode_digit,
+        tqdm_mode=1,
     )
 
-    # Testing
-    test_digit_recog_model(
-        class_am, ortho_im, DATA_DIR, NUM_CLASSES, NUM_FEATURES, NUM_TRAIN, NUM_TEST
+    print("Testing model...")
+    counts, scores, accuracies = test_model(
+        test_dataset=train_data,
+        ortho_im=ortho_im,
+        cim=None,
+        class_am=class_am,
+        encode_function=encode_digit,
+        staring_num_test=0,
+        num_test=NUM_TEST,
+        tqdm_mode=1,
+        print_mode=1,
     )
 
-    # Retraining
-    class_am, class_am_int, class_am_elem_count = retrain_digit_recog_model(
-        class_am,
-        class_am_int,
-        class_am_elem_count,
-        ortho_im,
-        DATA_DIR,
-        NUM_RETRAIN,
-        NUM_FEATURES,
-        0,
+    print("Retraining model...")
+    (
+        class_am_retrained,
+        class_am_int_retrained,
+        class_am_elem_count_retrained,
+    ) = retrain_model(
+        retrain_dataset=train_data,
+        num_retrain=NUM_RETRAIN,
+        ortho_im=ortho_im,
+        cim=None,
+        class_am=class_am,
+        class_am_int=class_am_int,
+        class_am_elem_count=class_am_elem_count,
+        encode_function=encode_digit,
+        tqdm_mode=1,
     )
 
-    # Testing
-    test_digit_recog_model(
-        class_am, ortho_im, DATA_DIR, NUM_CLASSES, NUM_FEATURES, NUM_TRAIN, NUM_TEST
+    print("Testing re-trained model...")
+    counts, scores, accuracies = test_model(
+        test_dataset=train_data,
+        ortho_im=ortho_im,
+        cim=None,
+        class_am=class_am_retrained,
+        encode_function=encode_digit,
+        staring_num_test=0,
+        num_test=NUM_TEST,
+        tqdm_mode=1,
+        print_mode=1,
     )
