65731e800d1b040c4f9477abcc95690f177f2258
rgantonio <ryan.antonio@esat.kuleuven.be>
2026-04-16 09:19:04 +0200

    app: update digit recog uint8

--- app/vsax_digit_recog.py
+++ app/vsax_digit_recog.py
@@ -5,25 +5,27 @@ This application demonstrates the use of VSAX
 for digit recognition using the MNIST dataset.
 """
 
-# Parameters
+# Packages
 import os
 import sys
 
+
 # Path directories
-curr_dir = os.getcwd()
+curr_dir = os.path.dirname(os.path.abspath(__file__))
 lib_path = curr_dir + "/../lib"
-extract_path = curr_dir + "/../data/extract_data"
 data_path = curr_dir + "/../data"
-dir_bin_data = data_path + "/mnist_uint"
+dataset_path = data_path + "/mnist_uint"
 
 # Appending other paths for libraries
 sys.path.append(lib_path)
-sys.path.append(extract_path)
 
 # Importing VSAX libraries
 import vsax  # noqa: E402
 import vsax_models  # noqa: E402
 import vsax_util  # noqa: E402
+
+save_mode, load_mode, model_name = vsax_models.vsax_general_parser()
+model_dir = curr_dir + f"/../models/{model_name}.npz"
 
 # Downloading and extracting the MNIST dataset
 vsax_util.download_and_extract(
@@ -36,17 +38,17 @@ vsax_util.download_and_extract(
 class_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Read data
-X_data = vsax_util.read_data(class_list, dir_bin_data)
+X_data = vsax_util.read_data(class_list, dataset_path)
 
 # Train and test split
 train_test_split = 0.6
 train_valid_split = 0.75
 
-X_train_set, X_test_set = vsax_util.split_data(
-    X_data, class_list, split_percent=train_test_split
-)
-X_train_set_src, X_valid_set = vsax_util.split_data(
-    X_train_set, class_list, split_percent=train_valid_split
+X_train_set, X_valid_set, X_test_set = vsax_util.split_train_valid_test_set(
+    X_data=X_data,
+    class_list=class_list,
+    train_test_split=train_test_split,
+    train_valid_split=train_valid_split,
 )
 
 
@@ -67,18 +69,27 @@ class digitVSA(vsax_models.vsaModel):
 
 # Make digit class
 digit_model = digitVSA(
+    model_name=model_name,
     hv_size=1024,
     class_list=class_list,
 )
 
-# Train the model
-digit_model.train_model(X_train_set_src)
+if load_mode:
+    # Load an existing trained model
+    digit_model.load_model(model_dir)
+else:
+    # Train the model
+    digit_model.train_model(X_train_set)
 
-# Retrain the model
-digit_model.retrain_model(X_valid_set)
+    # Retrain the model
+    digit_model.retrain_model(X_valid_set)
 
 # Test the model
 digit_model.test_model(X_test_set)
 
 # Print some statistics
 digit_model.print_model_stats()
+
+# Save model
+if save_mode:
+    digit_model.save_model(model_dir)
