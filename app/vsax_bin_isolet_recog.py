448c20dcfe895a158632067ba64b3181079880cf
Ryan Antonio <ryan.antonio@esat.kuleuven.be>
2026-04-27 14:08:02 +0200

    Add ISOLET program (#169)
    * app: update dna
    
    * lib: add conver levels
    
    * app: add isolet program
    
    * pixi and ci: add isolet check
    
    * app: fix pathing

--- app/vsax_bin_isolet_recog.py
+++ app/vsax_bin_isolet_recog.py
@@ -1,26 +1,27 @@
 """
-VSAX Binary DNA Recognition
+VSAX Binary ISOLET Recognition
 
 This application demonstrates the use of VSAX
-for the DNA recognition task.
+for the ISOLET recognition task.
 """
 
 # Parameters
-import os
 import sys
+import os
 from pathlib import Path
 
 # Global parameters
 HV_SIZE = 512
-CLASS_LIST = list(range(3))
+CLASS_LIST = list(range(26))
 GEN_TYPE = "lfsr"
+NUM_CIM = 21
 
 # Path directories
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 model_name = Path(__file__).stem
 lib_path = curr_dir + "/../lib"
 data_path = curr_dir + "/../data"
-dataset_path = data_path + "/dna"
+dataset_path = data_path + "/isolet"
 model_path = curr_dir + "/../models"
 
 # Appending other paths for libraries
@@ -48,15 +49,16 @@ if load_mode:
         filename=model_file,
     )
 
-# Downloading and extracting the DNA dataset
+# Downloading and extracting the ISOLET dataset
 vsax_util.download_and_extract(
-    url=vsax_util.vsax_data_url_bin_dna,
+    url=vsax_util.vsax_data_url_bin_isolet,
     out_dir=data_path,
     delete_archive=True,
 )
 
 # Read data
 X_data = vsax_util.read_data(CLASS_LIST, dataset_path, disable_tqdm=disable_tqdm)
+X_data = vsax_util.convert_levels(X_data, NUM_CIM)
 
 # Train and test split
 train_test_split = 0.6
@@ -71,8 +73,8 @@ X_train_set, X_valid_set, X_test_set = vsax_util.split_train_valid_test_set(
 )
 
 
-# Make class for DNA model
-class dnaVSA(vsax_models.vsaModel):
+# Make class for ISOLET model
+class isoletVSA(vsax_models.vsaModel):
     def encode(self, item_data):
         # Feature length
         item_len = len(item_data)
@@ -80,8 +82,17 @@ class dnaVSA(vsax_models.vsaModel):
 
         # Iterate through different item
         for i in range(item_len):
-            # Fetch and permute i-times
-            encoded_vec += vsax.hv_circ_perm(self.ortho_im[item_data[i]], i)
+            # Get ID HV
+            attribute_id_hv = self.ortho_im[i]
+            # Get value HV at that ID
+            attribute_val_hv = self.cim[item_data[i]]
+            # Bind ID and value HVs
+            attribute_val_loc_hv = vsax.hv_bind(
+                attribute_id_hv, attribute_val_hv, hv_type=self.hv_type
+            )
+            # Accumulate through all samples
+            encoded_vec += attribute_val_loc_hv
+
         # Threshold for binarization
         threshold = item_len // 2
         # Binarization
@@ -90,42 +101,43 @@ class dnaVSA(vsax_models.vsaModel):
         return encoded_vec
 
 
-# Make DNA class
-dna_model = dnaVSA(
+# Make ISOLET class
+isolet_model = isoletVSA(
     model_name=model_name,
     hv_size=HV_SIZE,
     class_list=CLASS_LIST,
     gen_type=GEN_TYPE,
+    num_cim=NUM_CIM,
 )
 
 # Binarize AM
-dna_model.binarize_am = True
+isolet_model.binarize_am = True
 
 # Disabling TQDM debug progress bars
-dna_model.tqdm_train_disable = disable_tqdm
-dna_model.tqdm_retrain_disable = disable_tqdm
-dna_model.tqdm_test_disable = disable_tqdm
+isolet_model.tqdm_train_disable = disable_tqdm
+isolet_model.tqdm_retrain_disable = disable_tqdm
+isolet_model.tqdm_test_disable = disable_tqdm
 
 if load_mode:
     # Load an existing trained model
-    dna_model.load_model(model_dir)
+    isolet_model.load_model(model_dir)
 else:
     # Train the model
-    dna_model.train_model(X_train_set)
+    isolet_model.train_model(X_train_set)
 
     # Retrain the model
-    dna_model.retrain_model(X_valid_set)
+    isolet_model.retrain_model(X_valid_set)
 
 # Test the model
-dna_model.test_model(X_test_set)
+isolet_model.test_model(X_test_set)
 
 # Print some statistics
-dna_model.print_model_stats()
+isolet_model.print_model_stats()
 
 # Checker if accuracy is expected
-assert dna_model.model_accuracy > 0.75, "Test accuracy is lower than expected."
+assert isolet_model.model_accuracy > 0.60, "Test accuracy is lower than expected."
 print("✓ Test accuracy passed.")
 
 # Save model
 if save_mode:
-    dna_model.save_model(model_dir)
+    isolet_model.save_model(model_dir)
